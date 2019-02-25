be74fadb94f20630ff2d74eb2e07f71fd16423e1
marcus110379 <marcusjmadden@gmail.com>
2019-02-25 13:08:33 -0600

    CHALLENGE BEGAN

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
-
+# URLs
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("welcomePerson/", views.welcomePerson, name="welcomePerson"),
+    path("addAmount/<int:personID>/", views.addAmount, name="addAmount"),
+    # Challenge
+    path("amountAdded/<int:amountID>/", views.amountAdded, name="amountAdded"),
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Account
 
 
 # Create your views here.
+
+# default function
 def index(request):
-    return HttpResponse("Test URL")
+    allPeople = Account.objects.all()
+    context = {
+        'allPeople': allPeople
+    }
+    return render(request, "accountApp/index.html", context)
+
+# Function that's print The person name we write on the input field
+def welcomePerson(request):
+    print(request.POST)
+    return HttpResponse("Welcome to the "+request.POST["personName"]+"'s account")
+
+# Function that add $5 on each person's checking account
+def addAmount(request, personID):
+    everyOne = get_object_or_404(Account, pk=personID)
+    everyOne.checking += 5
+    everyOne.save()
+    return redirect("index")
+
+# Challenge
+def amountAdded(request):
+    return HttpResponse(request.POST["amountToAdd"])
